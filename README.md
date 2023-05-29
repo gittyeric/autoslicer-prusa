0f7c828770546adb869d54ee399a5a3ec48d1c5c
gittyeric <git@letsmakeit.com>
2023-05-29 01:39:59 -0700

    Implement v1 CD for autoslicer

---
+++ README.md
@@ -0,0 +1,97 @@
+# autoslicer-prusa
+
+Never manually slice an STL again!  Upon saving your PrusaSlicer project file, generate all gcode permutations you care about and optionally auto-upload to all your printers. Example: update your model in 1 project file and autogenerate gcodes for many profiles like Printer1+PLA, Printer2+PLA, Printer1+PETG, Printer2+PETG.  This will also watch ALL your project files and global settings changes and instantly regenerate any affected gcode outputs and sync with printers.  Pretty much "hot reload" for fast design prototyping!  Hit save in PrusaSlicer and print any gcode version everywhere leveraging just your native PrusaSlicer settings.
+
+## How it works
+
+Given a projects folder that has some arbitrary structure, a mirror "gcode" folder will be generated that matches all your project files, but may contain extra gcode outputs suffixed with `"myProject/myPart-<Printer>-<Filament>-<PrintSettings>.gcode"`.  This script reads your PrusaSlicer's configuration directory automatically to generate all permutations of gcode you could ever care to generate, here's an example project folder:
+
+```
+<Root Project folder>
+- myProject1
+  - part1.3mf
+  - part2.3mf
+- myProject2
+  - part1.3mf
+  - part2.3mf
+```
+
+and would produce the following output structure:
+
+
+```
+<Root Project folder>
+- gcode
+  - myProject1
+    - part1-myPrinter1-pla-custom1.gcode
+    - part1-myPrinter2-pla-custom1.gcode
+    - part1-myPrinter1-petg-custom1.gcode
+    - part1-myPrinter2-petg-custom1.gcode
+    - part1-myPrinter1-petg-custom2.gcode
+    - part1-myPrinter2-petg-custom2.gcode
+    ... (More excluded)
+  - myProject2
+    - part1-myPrinter1-pla-custom1.gcode
+    - part1-myPrinter2-pla-custom1.gcode
+    - part1-myPrinter1-petg-custom1.gcode
+    - part1-myPrinter2-petg-custom1.gcode
+    - part1-myPrinter1-petg-custom2.gcode
+    - part1-myPrinter2-petg-custom2.gcode
+    ... (More excluded)
+```
+
+Updating your .ini files will regenerate everything and updating a 3mf file only regenerates all gcodes for that part.
+
+## Installing
+
+This program requires Node.js (version 16 or higher) running on a Bash-like terminal.  Windows users can use a number of Bash-like emulators.
+
+1. Clone this repository: `git clone https://github.com/gittyeric/autoslicer-prusa`
+
+2. With Node.js (version 16 or up should do) and NPM installed, you can install by changing to this repo's directory and running:
+
+`npm install`
+
+3. You can now run with `npm --prusa=<my-prusaslicer-config-location> --project="<my-absolute-projects-folder-location>" start`
+
+Where:
+
+`<my-prusaslicer-config-location>` is the location PrusaSlicer takes you to when navigating to Help -> Show Configuration Folder.
+`<my-absolute-projects-folder-location>` is the folder containing your PrusaSlicer .3mf project files (subfolders will also count).
+
+If you see issues such as "Command not found: prusa-slicer", change `prusaSlicerCmd` in `config.ts` to whatever command works for your terminal (ex. `prusa-slicer-console` or `slic3r`).
+
+## Running forever in Unix systems
+
+The easiest way to install this program to run 24/7 and listen for file changes in a lightweight way is to add the script you are successfully running in step 3 to your crontab with:
+
+`crontab -e`
+
+Then add an entry to run autoslicer after every reboot:
+
+`@reboot cd </path/to/>autoslicer-prusa && npm start --prusa=<my-prusaslicer-config-location> --project="<my-absolute-projects-folder-location>"`
+
+## Adding new (Printer / Filament / Print Settings) combinations
+
+This script will automatically track any changes or additions to your Printer, Filament or Print Settings profiles and regenerate all your project files accordingly.
+
+```
+Warning! Creating lots of .ini settings produces exponential permutations of gcodes per project file, so only keep the settings you really need in Prusaslicer and aggressively delete unused ones.
+```
+
+## Adding new 3mf project files
+
+Simply add a new .3mf file anywhere in your projects folder and it will be immediately sliced against all `settings/` permutations and show up in the gcode/ folder.
+
+## (Optional): Continous Delivery + "Live Reload" upload to printers
+
+Simply save a PrusaSlicer project to auto-upload to all your printers!
+
+Edit `config.ts` to include the list of SSH addresses to upload to (hint: you should use `ssh-copy-id` so your printer trusts your local Printer for SSH connections).  Whatever address(es) you use to access your printer(s) with ssh (ex. ssh `pi@myprinter.local`), add to the `rsyncUploadTargets` list.  Alternatively, you can specify a comma-separated `targets` argument on a per-command basis instead of modifying `config.ts`:
+
+`npm --targets="pi@printer1.local,pi@10.10.10.10" --prusa=<my-prusaslicer-config-location> --project="<my-absolute-projects-folder-location>" start`
+
+
+## Octoprint End-to-end Automation
+
+If you use Octoprint or Linux to run your printer, you can also tie the gcode generation event to auto-upload the file to all your Octoprint bots using it's built-in folder watch feature.  By default, Octoprint checks for incoming gcode files (after [manually enabling this feature](https://community.octoprint.org/t/watched-folder-doesnt-run-as-well/14618/4)) at `/home/pi/.octoprint/watched`, so you can use the above section to configure appropriate targets such as `pi@10.10.10.10:/home/pi/.octoprint/watched`
---
+++ index.ts
@@ -0,0 +1,327 @@
+import watch from "node-watch";
+import fs from 'fs';
+import path from 'path';
+import { ExecaChildProcess, execa } from 'execa';
+
+export const prusaSlicerCmd: string = 'prusa-slicer';
+
+// Add any targets that your machine can successfully access with ssh, followed by :<absolute-remote-folder-location>
+export const rsyncUploadTargets: string[] = [
+    //'pi@myprinter.local:/home/pi/.octoprint/watched',
+    //'pi@10.10.10.10:/home/pi/.octoprint/watched',
+];
+
+type Settings = {
+    prusaFolder: string,
+    printers: string[],
+    filaments: string[],
+    printSettings: string[],
+}
+
+type GcodeMeta = {
+    file: string,
+    projectSrc: string,
+    settings: [string, string, string],
+}
+
+let generateAllLock: Promise<unknown> = Promise.resolve(0);
+let generateAllRequestCount = 0;
+let generateAllCompleteCount = 0;
+
+function* walk3mfSync(dir: string): Generator<string, undefined, undefined> {
+    const files = fs.readdirSync(dir, { withFileTypes: true });
+    for (const file of files) {
+        if (file.isDirectory()) {
+            yield* walk3mfSync(path.join(dir, file.name));
+        } else {
+            const fullFile = path.join(dir, file.name);
+            if (fullFile.endsWith('.3mf')) {
+                yield fullFile;
+            }
+        }
+    }
+    return undefined;
+}
+
+function* walkGcodesSync(gcodeDir: string) {
+    const files = fs.readdirSync(gcodeDir, { withFileTypes: true });
+    for (const file of files) {
+        if (file.isDirectory()) {
+            yield* walk3mfSync(path.join(gcodeDir, file.name));
+        } else {
+            const fullFile = path.join(gcodeDir, file.name);
+            if (fullFile.endsWith('.gcode')) {
+                yield fullFile;
+            }
+        }
+    }
+}
+
+function getSettings(prusaFolder: string): Settings {
+    const printers = fs.readdirSync(prusaFolder + '/printer', { withFileTypes: true }).filter((f) => f.name.endsWith('.ini')).map((f) => f.name.replace(/\.ini/g, ''));
+    const filaments = fs.readdirSync(prusaFolder + '/filament', { withFileTypes: true }).filter((f) => f.name.endsWith('.ini')).map((f) => f.name.replace(/\.ini/g, ''));
+    const printSettings = fs.readdirSync(prusaFolder + '/print', { withFileTypes: true }).filter((f) => f.name.endsWith('.ini')).map((f) => f.name.replace(/\.ini/g, ''));
+
+    return { prusaFolder, printers, filaments, printSettings };
+}
+
+async function execLogError(cmd: string, args: string[]) {
+    try {
+        console.info('autoslice: ' + cmd + ' ' + args.join(' '));
+        const res = await execa(cmd, args);
+        console.info('autoslice: ' + res.stdout);
+    } catch (e) {
+        console.error('autoslice: ' + JSON.stringify(e, null, 2));
+    }
+}
+
+function generateCmd(settings: Settings, gcodeMeta: GcodeMeta) {
+    let args = ['-o', gcodeMeta.file, '-g', gcodeMeta.projectSrc];
+    for (let i = 0; i < gcodeMeta.settings.length; i++) {
+        const setting = gcodeMeta.settings[i];
+        if (setting !== '_') {
+            const folder = i === 0 ? 'printer' : (i === 1 ? 'filament' : 'print');
+            args.push(...['--load', `${settings.prusaFolder}/${folder}/${setting}.ini`]);
+        }
+    }
+
+    return execLogError(prusaSlicerCmd, args);
+}
+
+function findAllGcodePermutations(projectsFolder: string, settings: Settings, file3mf: string): GcodeMeta[] {
+    const metas: GcodeMeta[] = [];
+    // Generate all the cmds to run to generate all the permutations this file needs
+    // First, generate the vanilla project file as-is
+    const baseGcodeFile = file3mf.replace(/3mf$/g, 'gcode').replace(projectsFolder, projectsFolder + '/gcode');
+    metas.push({
+        file: baseGcodeFile,
+        projectSrc: file3mf,
+        settings: ['_', '_', '_'],
+    });
+
+    // Now all the explicit permutations
+    const level1Settings = settings.printers || ['_'];
+    const level2Settings = settings.filaments || ['_'];
+    const level3Settings = settings.printSettings || ['_'];
+
+    for (const level1 of level1Settings) {
+        for (const level2 of level2Settings) {
+            for (const level3 of level3Settings) {
+                if (level1 === '_' && level2 === '_' && level3 === '_') {
+                    continue;
+                }
+                metas.push({
+                    file: baseGcodeFile.replace(/\.gcode$/g, `_${level1}-${level2}-${level3}.gcode`),
+                    projectSrc: file3mf,
+                    settings: [level1, level2, level3],
+                });
+            }
+        }
+    }
+
+    return metas;
+}
+
+async function generateAllForProject(projectsFolder: string, settings: Settings, file3mf: string) {
+    console.info('autoslice: ' + `Slicing ${file3mf}`);
+    const gcodeMetas = findAllGcodePermutations(projectsFolder, settings, file3mf);
+    const pendingSlices = gcodeMetas.map(async (gcodeMeta) => {
+        // Ensure directory exists recursively
+        fs.mkdirSync(path.dirname(gcodeMeta.file), { recursive: true });
+        return generateCmd(settings, gcodeMeta);
+    });
+    try {
+        await Promise.all(pendingSlices);
+    } catch (e) {
+        console.error('autoslice: ' + JSON.stringify(e));
+    }
+
+    return gcodeMetas;
+}
+
+function _deleteAllGcodes(gcodeFolder: string) {
+    if (fs.existsSync(gcodeFolder)) {
+        console.warn('autoslice: ' + `Deleting all gcode in ${gcodeFolder}`);
+        fs.rmSync(gcodeFolder, { recursive: true, force: true });
+        fs.mkdirSync(gcodeFolder);
+        return true;
+    }
+    return false;
+}
+
+// Destroy and re-create the entire projectsFolder, guaranteed to only run 1 at a time
+async function generateAll(projectsFolder: string, settings: Settings) {
+    // Don't even bother if too many pending requests to regenerate all
+    if (generateAllRequestCount - 1 > generateAllCompleteCount) {
+        console.warn('Throttling nuclear regenerate and upload');
+        return;
+    }
+
+    generateAllRequestCount++;
+    // Ensure generateAll can only be called once at a time!
+    generateAllLock = generateAllLock.then(() => {
+        return new Promise(async (res) => {
+            _deleteAllGcodes(projectsFolder + '/gcode');
+            // Walk all files that end with .3mf
+            const projectFiles = walk3mfSync(projectsFolder);
+            for (const projectFile of projectFiles) {
+                await generateAllForProject(projectsFolder, settings, projectFile);
+            }
+            await triggerRsyncUploads(projectsFolder + '/gcode');
+            generateAllCompleteCount++;
+            res(undefined);
+        });
+    });
+}
+
+// Leave the smarts of diff tracking to rsync and just bulk rsync
+// all generated gcode to all rsync targets
+async function triggerRsyncUploads(gcodeFolder: string) {
+    const pendingUploads = rsyncUploadTargets.map(async (target) => {
+        const targetWrite = execLogError('scp', ['-r', gcodeFolder + '/', target]);
+        targetWrite.then(() => { console.info(`autoslice: Done full-syncing with ` + target) });
+        return targetWrite;
+    });
+    try {
+        await Promise.all(pendingUploads);
+    } catch (e) {
+        console.error('autoslice: ' + JSON.stringify(e));
+    }
+}
+
+async function triggerRsyncUpload(projectsFolder, gcodeFile: string) {
+    console.log('' + JSON.stringify(projectsFolder, null, 2));
+    console.log('' + JSON.stringify(gcodeFile, null, 2));
+    const destFolder = path.dirname(gcodeFile).replace(projectsFolder + '/gcode', '');
+    console.log('' + JSON.stringify(destFolder, null, 2));
+    const pendingUploads = rsyncUploadTargets.map(async (target) => {
+        const targetWrite = execLogError('rsync', [gcodeFile, (target + destFolder).replace('//', '/')]);
+        return targetWrite;
+    });
+    try {
+        await Promise.all(pendingUploads);
+    } catch (e) {
+        console.error('autoslice: ' + JSON.stringify(e));
+    }
+}
+
+function findStaleGcodes(settings: Settings, projectsFolder: string, updatedProjectFile: string): string[] {
+    const updatedDir = path.dirname(updatedProjectFile);
+    const projectName = path.basename(updatedProjectFile).replace(/.3mf$/g, '');
+    const updatedGcodeDir = updatedDir.replace(projectsFolder, projectsFolder + '/gcode');
+
+    // If gcode dir doesn't event exist yet, no stale files
+    if (!fs.existsSync(updatedGcodeDir)) {
+        return [];
+    }
+    const gcodeFiles = fs.readdirSync(updatedGcodeDir, { withFileTypes: true });
+    const regex = new RegExp(`${projectName}_.*\-.*\-.*.gcode`);
+    return gcodeFiles.filter((gf) => {
+        gf.isFile() && regex.test(gf.name)
+    }).map((gf) => path.join(gf.path, gf.name));
+}
+
+function watchProjectsDir(projectsFolder: string, prusaFolder: string): void {
+    const watcher = watch.default(projectsFolder, {
+        recursive: true,
+        delay: 250,
+        filter(file, skip) {
+            return file.endsWith('.3mf');
+        },
+    }, async function (evt, updatedFilename) {
+        const settings = getSettings(prusaFolder);
+        // Unconditionally delete stale gcode files
+        const staleFiles = findStaleGcodes(settings, projectsFolder, updatedFilename);
+        for (const stale of staleFiles) {
+            console.info('autoslice: ' + `Deleting stale ${stale}`);
+            fs.unlinkSync(stale);
+        }
+        if (evt === 'update') {
+            // If project file updated/added, regenerate
+            const gcodeFiles = (await generateAllForProject(projectsFolder, settings, updatedFilename)).map((g) => g.file);
+            for (const gcodeFile of gcodeFiles) {
+                triggerRsyncUpload(projectsFolder, gcodeFile);
+            }
+        }
+    });
+    console.info('autoslice: ' + `Listening for .3mf files in ${projectsFolder}`);
+    //process.on('SIGINT', watcher.close);
+    //process.on('SIGTERM', watcher.close);
+}
+
+function watchSettingsDir(projectsFolder: string, prusaFolder: string): void {
+    const watcher = watch.default(prusaFolder, {
+        recursive: true,
+        delay: 250,
+        filter(file, skip) {
+            return file.endsWith('.ini');
+        },
+    }, async function (evt, updatedFilename) {
+        // If removing a .ini file, just ignore
+        if (evt === 'remove') {
+            return;
+        }
+        if (updatedFilename.startsWith(prusaFolder + '/printer/') ||
+            updatedFilename.startsWith(prusaFolder + '/print/') ||
+            updatedFilename.startsWith(prusaFolder + '/filament/')) {
+            console.info('autoslice: ' + `Regenerating all in response to ${updatedFilename} changing`);
+            const settings = getSettings(prusaFolder);
+            // Delete all and regenerate some if global settings changed at all
+            await generateAll(projectsFolder, settings);
+        }
+    });
+    console.info('autoslice: ' + `Listening for .ini files in ${prusaFolder}`);
+}
+
+// Entrypoint
+(async () => {
+    const rawProjectsFolder = process.env.npm_config_project as string;
+    const projectsFolder = rawProjectsFolder.endsWith('/') ? rawProjectsFolder.substring(0, rawProjectsFolder.length - 1) : rawProjectsFolder;
+    if (!fs.existsSync(projectsFolder)) {
+        console.error('autoslice: ' + `Could not find project directory ${projectsFolder}, please specify project folder like: 'npm --project="<absolute-path-to-projects>" start'`);
+        process.exit(-1);
+    }
+
+    // Ensure gcode output dir exists
+    if (!fs.existsSync(projectsFolder + '/gcode')) {
+        fs.mkdirSync(projectsFolder + '/gcode');
+    }
+
+    // Ensure all settings folders are good
+    const rawPrusaFolder = process.env.npm_config_prusa as string;
+    const prusaFolder = rawPrusaFolder.endsWith('/') ? rawPrusaFolder.substring(0, rawPrusaFolder.length - 1) : rawPrusaFolder;
+    if (!fs.existsSync(prusaFolder)) {
+        console.error('autoslice: ' + `Could not find PrusaSlicer directory ${prusaFolder}, please check permissions or specify project folder like: 'npm --prusa="<absolute-path-to-Prusa/.config/PrusaSlicer-beta>" start'`);
+        process.exit(-1);
+    }
+    if (!fs.existsSync(prusaFolder + '/printer')) {
+        console.error('autoslice: ' + `Expected a printer/ directory in ${prusaFolder}, wrong directory?`);
+        process.exit(-1);
+    }
+    if (!fs.existsSync(prusaFolder + '/print')) {
+        console.error('autoslice: ' + `Expected a print/ directory in ${prusaFolder}`);
+        process.exit(-1);
+    }
+    if (!fs.existsSync(prusaFolder + '/filament')) {
+        console.error('autoslice: ' + `Expected a filament/ directory in ${prusaFolder}`);
+        process.exit(-1);
+    }
+
+    // Optional per-run upload targets are merged with the same list from config.ts
+    const rawTargets = process.env.npm_config_targets;
+    const targets = rawTargets ? rawTargets.split(',') : [];
+    if (targets.length > 0) {
+        rsyncUploadTargets.push(...targets);
+        console.info('autoslice: ' + `Using upload targets: ${JSON.stringify(rsyncUploadTargets, null, 2)}`);
+    }
+
+    const settings = getSettings(prusaFolder);
+    console.info('autoslice: ' + `Loaded settings combinations: ${JSON.stringify(settings, null, 2)}`);
+
+    // Listen to changes forever
+    watchProjectsDir(projectsFolder, prusaFolder);
+    watchSettingsDir(projectsFolder, prusaFolder);
+
+    // Ensure all is generated since last run
+    await generateAll(projectsFolder, settings);
+})();
